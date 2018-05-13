3381c5f7e1381e6b0725999cb6e6f3a250a86e94
Lucas Klassmann <lucasklassmann@gmail.com>
2018-05-12 23:58:27 -0300

    Improving InfoWindow UI

--- src/map/InfoWindow.js
+++ src/map/InfoWindow.js
@@ -10,7 +10,9 @@ export class InfoWindow extends React.Component {
             title: this.props.title,
             detail: '',
             twitter: '',
-            url: ''
+            url: '',
+            phone: '',
+            phoneFormatted: ''
         }
     }
 
@@ -20,11 +22,35 @@ export class InfoWindow extends React.Component {
         // console.log(resp);
         place.getDetail().then((response) => {
             console.log(response);
+            let venue = response.response.venue;
+
+            let { name } = venue;
+            let { description } = venue;
+            let { twitter } = venue.contact;
+            let { instagram } = venue.contact;
+            let { facebookUsername } = venue.contact;
+            let { phone } = venue.contact;
+            let { url } = venue;
+            let { formattedPhone } = venue.contact;
+            let { bestPhoto } = venue;
+            let { prefix } = bestPhoto;
+            let { suffix } = bestPhoto;
+            var photoUrl = null;
+
+            if (prefix && suffix) {
+                photoUrl = `${prefix}original${suffix}`;
+            }
+
             this.setState({
-                title: response.response.venue.name,
-                detail: response.response.venue.description,
-                twitter: response.response.venue.contact.twitter,
-                url: response.response.venue.url
+                title: name,
+                description: description,
+                twitter: twitter,
+                facebook: facebookUsername,
+                instagram: instagram,
+                url: url,
+                phone: phone,
+                formattedPhone: formattedPhone,
+                photo: photoUrl
             });
         });
     }
@@ -56,26 +82,51 @@ export class InfoWindow extends React.Component {
 
     renderInfoWindow() {
         let { google } = this.props;
+
         let { title } = this.state;
-        let { detail } = this.state;
+        let { description } = this.state;
         let { twitter } = this.state;
+        let { facebook } = this.state;
+        let { instagram } = this.state;
         let { url } = this.state;
+        let { phone } = this.state;
+        let { formattedPhone } = this.state;
+        let { photo } = this.state;
+
 
         var template = `
             <h2>${title}</h2>
         `;
 
-        if (detail) {
-            template += `<p>${detail}</p>`;
+        if (photo) {
+            template += `<img style="width: 200px;" src="${photo}" />`;
+        }
+
+        if (description) {
+            template += `<p>${description}</p>`;
+        }
+
+        if (facebook) {
+            template += `<a target="_blank" href="https://fb.com/${facebook}"><i class="fab fa-facebook-square"></i></a>`;
+        }
+
+        if (instagram) {
+            template += `<a target="_blank" href="https://instagram.com/${instagram}"><i class="fab fa-instagram"></i></a>`;
         }
 
         if (twitter) {
-            template += `<p>Twitter: <a target="_blank" href="https://twitter.com/${twitter}">@${twitter}</a></p>`;
+            template += `<a target="_blank" href="https://twitter.com/${twitter}"><i class="fab fa-twitter"></i></a>`;
         }
 
         if (url) {
             template += `<p>Website: <a target="_blank" href="${url}">${url}</a></p>`;
         }
+
+        var templateContainer = `
+        <div style="width: 300px;">
+            ${template}
+        </div>
+        `;
 
         this.infowindow = new google.maps.InfoWindow({
             content: template
